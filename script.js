1a35fc9cfd24166d4eb46638ccd4760644328f16
Anderson Santos <andiinps@gmail.com>
2024-10-03 17:10:36 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-
-
-
 const adminUser = "admin"
-const adminPassword = "1234"  
-
+const adminPassword = "1234"
 
 function realizarLogin() {
-   
     const username = document.getElementById('username').value
     const password = document.getElementById('password').value
 
-    
     if (username === adminUser && password === adminPassword) {
-        
         document.getElementById('admin-login').style.display = 'none'
         document.getElementById('admin-section').style.display = 'block'
     } else {
-        
         document.getElementById('login-error').style.display = 'block'
     }
 }
-
 
 function atualizarCard() {
     const cardNumber = document.getElementById('card-number').value
@@ -43,8 +34,35 @@ function atualizarCard() {
     }
 }
 
+function adicionarNovoCard() {
+    const newCardTitle = document.getElementById('new-card-title').value
+    const newCardDescription = document.getElementById('new-card-description').value
+    const newCardImage = document.getElementById('new-card-image').value
 
+    // Criar um novo card HTML
+    const newCard = document.createElement('div')
+    newCard.classList.add('card')
+
+    newCard.innerHTML = `
+        <img src="${newCardImage}" alt="${newCardTitle}">
+        <h3>${newCardTitle}</h3>
+        <p>${newCardDescription}</p>
+        <button class="btn-card">Saiba Mais</button>
+    `
+
+    // Adicionar o novo card à seção de serviços na index.html
+    const cardContainer = document.querySelector('.card-container')
+    cardContainer.appendChild(newCard)
+
+    alert('Novo card adicionado com sucesso!')
+
+    // Limpar os campos do formulário
+    document.getElementById('new-card-title').value = ''
+    document.getElementById('new-card-description').value = ''
+    document.getElementById('new-card-image').value = ''
+}
+
+// Event Listeners
 document.getElementById('login-button').addEventListener('click', realizarLogin)
-
-
 document.getElementById('update-card').addEventListener('click', atualizarCard)
+document.getElementById('add-new-card').addEventListener('click', adicionarNovoCard)
