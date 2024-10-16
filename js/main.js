a503543ead074a6a639f87a3c031850096475d6e
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-10-15 21:40:14 -0300

    ajust

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ function navBarComponent() {
                 <li><a href="index.html#services">Serviços</a></li>
                 <li><a href="index.html#contact">Contato</a></li>
                 <li><a href="login.html">Login</a></li>
-                <li><a href="cadastro.html">Cadastro</a></li>
             </ul>
         </nav>`
 
