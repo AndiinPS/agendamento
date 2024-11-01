f2bbb435b7408433b58eee4cd2b28dff06deb062
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2024-10-31 21:54:41 -0300

    regex

---
+++ js/validator.js
@@ -0,0 +1,82 @@
+document.addEventListener('DOMContentLoaded', () => {
+    validaNome();
+    validaEmail();
+    validaTelefone();
+    validateRequired();  
+});
+
+function validaNome() {
+
+    const element = document.querySelector("#nomeInput");
+    const message = document.querySelector("#nomeHelp");
+    element.addEventListener('blur' , (e) => {
+        const value = e.target.value;
+        let error = ''
+        
+        
+        if (value == '' ) {
+
+            error = '<span class="text-danger"> Preencha o campo de maneira correta! </span>';
+
+        }
+
+        message.innerHTML = error;
+       
+    });    
+
+}
+
+function validaEmail() {
+
+    const element = document.querySelector("#emailInput");
+    const message = document.querySelector("#emailHelp");
+    element.addEventListener('keyup' , (e) => {
+        const value = e.target.value;
+        let error = ''
+        const regexEmail = (/^[a-z0-9]+\@[a-z]+\.([a-z]{3})+$/g) 
+
+        if (!regexEmail.test(value)) {
+
+            error = '<span class="text-danger">Insira um e-mail válido!</span>';
+
+        }
+        
+        message.innerHTML = error;
+
+    });    
+      
+}
+
+function validaTelefone() {
+
+    const element = document.querySelector("#telefoneInput");
+    const message = document.querySelector("#telHelp");
+    element.addEventListener('keyup' , (e) => {
+        const value = e.target.value;
+        let error = ''
+        const regexTel = /^\([0-9]{2,3}\)[0-9]{5}-[0-9]{4}$/g
+
+        if (!regexTel.test(value)) {
+
+            error = '<span class="text-danger">Insira telefone valido! Formato (xx) xxxxx-xxxx </span>';
+
+        }
+        
+        message.innerHTML = error;
+
+    });    
+
+
+}
+
+function validateRequired() {
+    const elements = document.querySelectorAll("[required]")
+    const message = document.querySelector("#nameHelp")
+    console.log(elements)
+} 
+        
+      
+
+
+
+        
